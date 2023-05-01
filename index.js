b3b75ef2b523adb20b1658ac2807dfdb7eac30a0
Asher09-11 <124516801+Asher09-11@users.noreply.github.com>
2023-05-01 16:24:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import soalMudahRoute from "./routes/soalMudahRoute.js";
 import soalMenengahRoute from "./routes/soalMenengahRoute.js";
 import soalSulitRoute from "./routes/soalSulitRoute.js";
+import highscoreRoute from "./routes/highscoreRoute.js";
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use(soalMudahRoute);
 app.use(soalMenengahRoute);
 app.use(soalSulitRoute);
+app.use(highscoreRoute);
 
 app.listen(5000, () => console.log("Server running on port 5000"));
